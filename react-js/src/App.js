1dff2f731bc4cd21150042821f80f0455f7f9a70
RamVithyaKishor-D <ramvithyakishor007@gmail.com>
2023-11-08 17:41:26 +0530

    first project

---
+++ react-js/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './day2/Components/Navbar'
+import Footer from './day2/Components/Footer'
+import Main from './day2/Components/Main' 
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
